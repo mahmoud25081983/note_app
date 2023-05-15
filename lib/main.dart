8ee714bda969bd4882de70fac4e09538658d524d
Mahmoud <mahmoud1528346@gmail.com>
2023-05-15 20:04:44 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'views/notes_view.dart';
+
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'To Do',
+      theme: ThemeData(brightness: Brightness.dark
+        
+      ),
+      home: const NotesView(),
+    );
+  }
+}
+
+
