05e418f71da06c5be979305c34a4f7ee2efcf461
Mahmoud <mahmoud1528346@gmail.com>
2023-05-20 01:25:01 +0300

    create cubit & add note state & register adapter

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -13,3 +13,4 @@ class AddNoteFailure extends AddNoteState {
   final String errorMessage;
   AddNoteFailure(this.errorMessage);
 }
+//git config http.postBuffer 524288000
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'constant.dart';
+import 'models/notemodel.dart';
 import 'views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
 
- await Hive.openBox(kNotesBox);
-
+  await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
