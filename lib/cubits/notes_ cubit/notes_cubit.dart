05d2b6cbf9ead15c29fb7f278ca5ec38355756c9
Mahmoud <mahmoud1528346@gmail.com>
2023-05-22 02:38:47 +0300

    create fetch notes mothode

--- lib/cubits/notes_ cubit/notes_cubit.dart
+++ lib/cubits/notes_ cubit/notes_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
+import '../../constant.dart';
 import '../../models/notemodel.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() {
+     emit(NoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+    List<NoteModel> noteList =  noteBox.values.toList();
+      emit(NoteSuccess(noteList));
+    } on Exception catch (e) {
+      emit(NoteFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/notes_%20cubit/notes_cubit.dart';
 
 import 'widgets/custome_button_sheet.dart';
 import 'widgets/notes_view_body.dart';
@@ -8,23 +10,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-            context: context,
-            builder: (context) {
-              return const AddNoteButtonSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider<NotesCubit>(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25)),
+              context: context,
+              builder: (context) {
+                return const AddNoteButtonSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
 }
-
-
