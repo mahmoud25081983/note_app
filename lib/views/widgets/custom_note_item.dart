70e97a25b8650182c94c0a8a8dfb309e85448dfe
Mahmoud <mahmoud1528346@gmail.com>
2023-05-22 04:33:21 +0300

    display data part 2

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/models/notemodel.dart';
 import 'package:noteapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key, required this.noteModel});
 
+  final NoteModel noteModel;
+
+    final colors = const [
+    Colors.redAccent,
+    Colors.yellowAccent,
+    Colors.greenAccent,
+    Colors.blueAccent,
+    Colors.tealAccent,
+    Colors.orangeAccent,
+  ];
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -20,21 +31,21 @@ class NoteItem extends StatelessWidget {
         margin: const EdgeInsets.only(bottom: 4),
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-            color: const Color(0xffffcc80),
+            color: Color(noteModel.color),
             borderRadius: BorderRadius.circular(16)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
               //contentPadding: EdgeInsets.all(0),
-              title: const Text(
-                "Flutter Tips",
+              title:  Text(
+                noteModel.title,
                 style: TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16),
                 child: Text(
-                  "Build your Career With Tharwt Samy",
+                  noteModel.subTitle,
                   style: TextStyle(
                       color: Colors.black.withOpacity(0.6), fontSize: 18),
                 ),
@@ -51,7 +62,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24, top: 16),
               child: Text(
-                "May 21,2022",
+                noteModel.date,
                 style: TextStyle(
                     color: Colors.black.withOpacity(0.6), fontSize: 16),
               ),
--- lib/views/widgets/custome_list_view_builder.dart
+++ lib/views/widgets/custome_list_view_builder.dart
@@ -8,14 +8,7 @@ import 'custom_note_item.dart';
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
-  final Data = const [
-    Colors.redAccent,
-    Colors.yellowAccent,
-    Colors.greenAccent,
-    Colors.blueAccent,
-    Colors.tealAccent,
-    Colors.orangeAccent,
-  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +22,7 @@ class NotesListView extends StatelessWidget {
               padding: const EdgeInsets.all(0),
               itemCount: notes.length,
               itemBuilder: (BuildContext context, int index) {
-                return const NoteItem();
+                return  NoteItem(noteModel: notes[index]);
               },
             ),
           ),
